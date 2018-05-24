80d52ccb89921639d1a6c049b6bc66b7ada3afbe
Ge Yang <yangge1987@gmail.com>
2018-05-23 17:53:30 -0700

    Fix version (Hew...)

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+with open(path.join(path.abspath(path.dirname(__file__)), 'VERSION'), encoding='utf-8') as f:
+    version = f.read()
 
 setup(name='playground',
       packages=['playground'],
@@ -23,7 +29,8 @@ setup(name='playground',
           'click',
       ],
       description='A Playground for RL and Robotics Environments',
+      long_description=long_description,
       author='Ge Yang',
       url='https://github.com/episodeyang/rl-playground',
       author_email='yangge1987@gmail.com',
-      version='0.0.0')
+      version=version)
